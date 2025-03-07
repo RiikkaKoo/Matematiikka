4410b049c4026fb580109c7f3c787aba013c4dee
riikka <riikaka@metropolia.fi>
2025-03-07 21:50:48 +0200

    Ma3 osa 6

--- Ma3_osa6/matriisin_kaanteismatriisi.py
+++ Ma3_osa6/matriisin_kaanteismatriisi.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sympy import solve, symbols
 
 matriisi1 = np.array([[3, 2, 0, -4],
                       [5, 6, 7, 1],
@@ -8,3 +9,20 @@ matriisi1 = np.array([[3, 2, 0, -4],
 kaanteis1 = np.linalg.inv(matriisi1)
 print()
 print(kaanteis1)
+
+matriisi2 = np.array([[7, -3, -1],
+                      [-4, 9, 2],
+                      [1, -2, 0]])
+
+kaanteis2 = np.linalg.inv(matriisi2)
+print()
+print(kaanteis2)
+
+print()
+x, y, z = symbols('x, y, z')
+vastaus = solve([7*x - 3*y -1*z - 6,
+       -4*x + 9*y + 2*z - 5,
+           1*x - 2*y - -8 ], [x,y,z])
+
+print(vastaus)
+print(float(vastaus[x]), float(vastaus[y]), float(vastaus[z]))
